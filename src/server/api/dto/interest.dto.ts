47c80d8cfee293d5c8ea986484a63c7fcc545afb
Nat Silprasert <natsilprasert@gmail.com>
2025-10-26 00:50:15 +0700

    [feat] interest request schema

---
+++ src/server/api/dto/interest.dto.ts
@@ -0,0 +1,15 @@
+import type { interest } from "@/server/db/interest";
+import { z, type ZodSchema } from "zod";
+
+export type Interest = typeof interest.$inferInsert;
+
+export type CreateInterestRequest = Omit<typeof interest.$inferInsert, "id" | "createdAt" | "updatedAt">;
+
+export const CreateInterestRequestSchema: ZodSchema<CreateInterestRequest> = z.object({
+    name: z.string().min(1),
+});
+
+export const UpdateInterestRequestSchema = z.object({
+    id: z.number(),
+    name: z.string().optional(),
+});
