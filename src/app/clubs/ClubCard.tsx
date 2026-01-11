98a9fd3054157556b27c026e081716be6f4f4a42
Nat Silprasert <natsilprasert@gmail.com>
2026-01-11 16:55:05 +0700

    [feat] make ui responsive

--- src/app/clubs/ClubCard.tsx
+++ src/app/clubs/ClubCard.tsx
@@ -19,35 +19,42 @@ const ClubCard = (props: ClubCardProps) => {
     const { name, description, tag, imageUrl, categories } = props;
 
     return (
-        <div className='w-full flex flex-col p-4 gap-4 bg-white border border-stroke rounded-[12px]'>
+        <div className='w-full flex flex-col px-2.5 py-4 sm:p-4 gap-3 sm:gap-4 bg-white border border-stroke rounded-[12px]'>
             <div className='w-full flex items-center justify-between'>
                 {/* Left */}
-                <div className='flex gap-3 items-center'>
+                <div className='flex flex-col sm:flex-row gap-1 sm:gap-3 items-center w-full'>
                     <Image width={48} height={48} alt="club-profile" src={imageUrl} />
                     <div className='flex flex-col'>
                         <p className='font-semibold'>{name}</p>
                         <p className='text-text-gray text-[10px] sm:text-sm'>{tag}</p>
                     </div>
+
+                    {/* Description for mobile*/}
+                    <p className='sm:hidden text-text-gray text-center text-[10px] w-full'>
+                        {description}
+                    </p>
                 </div>
                 
                 {/* Right */}
-                <button className='flex gap-1 cursor-pointer px-2.5 py-1.5 rounded-full border-2 border-primary bg-white hover:bg-primary text-primary hover:text-white transition-all'>
+                <button 
+                    className='hidden lg:flex items-center gap-1 cursor-pointer px-2.5 py-1.5 rounded-full border-2 border-primary bg-white hover:bg-primary text-primary hover:text-white transition-all'
+                >
                     <p>ติดตาม</p>
-                    <Plus />
+                    <Plus size={16}/>
                 </button>
             </div>
 
             {/* Description */}
-            <p className='text-text-gray text-[10px] sm:text-sm w-full'>
+            <p className='hidden sm:flex text-text-gray text-sm w-full'>
                 {description}
             </p>
 
             {/* Categories */}
-            <div className='flex gap-2'>
+            <div className='flex gap-1 sm:gap-2 w-full flex-wrap items-center justify-center sm:justify-start'>
             {categories.map((category, idx) => (
                 <div 
                     key={idx} 
-                    className="font-semibold text-[8px] sm:text-xs rounded-[4px] text-white px-3 py-1"
+                    className="font-semibold text-[8px] sm:text-xs rounded-[4px] text-white px-2 sm:px-3 py-1"
                     style={{
                         backgroundColor: category.color,
                     }}
@@ -56,6 +63,14 @@ const ClubCard = (props: ClubCardProps) => {
                 </div>
             ))}
             </div>
+
+            {/* Follow Button for tablet and mobile*/}
+            <button 
+                className='flex items-center justify-center lg:hidden gap-1 cursor-pointer px-2.5 py-1 sm:py-1.5 rounded-full border-2 border-primary bg-white hover:bg-primary text-primary hover:text-white transition-all'
+            >
+                <p className='text-sm sm:text-base'>ติดตาม</p>
+                <Plus size={16} />
+            </button>
         </div>
     )
 }
--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -90,7 +90,7 @@ const ClubsPage = () => {
     return (
         <section className='body-section'>
             <h1>สำรวจชมรม</h1>
-            <div className='flex flex-col gap-6 w-full'>
+            <div className='flex flex-col gap-4 sm:gap-6 w-full'>
                 {/* Top Section */}
                 <div className='flex gap-4 w-full'>
                     <SearchBar className='flex-1' />
@@ -98,7 +98,7 @@ const ClubsPage = () => {
                 </div>
                 
                 {/* Clubs Section */}
-                <div className='grid grid-cols-3 gap-6'>
+                <div className='grid grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6'>
                     {data.map((club, idx) => (
                         <ClubCard key={idx} {...club} />
                     ))}
