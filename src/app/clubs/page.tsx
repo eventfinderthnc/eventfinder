68d17130f2986eb3d970e492313d8501847c5d61
Nat Silprasert <natsilprasert@gmail.com>
2026-01-11 17:15:12 +0700

    [feat] add filter icon

--- src/app/clubs/page.tsx
+++ src/app/clubs/page.tsx
@@ -1,7 +1,10 @@
+"use client"
+
 import { SearchBar } from '@/components/ui/SearchBar'
-import React from 'react'
+import React, { useState } from 'react'
 import type { ClubCardProps } from './ClubCard'
 import ClubCard from './ClubCard'
+import { SlidersHorizontal } from 'lucide-react'
 
 const data: ClubCardProps[] = [
     {
@@ -87,15 +90,31 @@ const data: ClubCardProps[] = [
 ]
 const ClubsPage = () => {
 
+    const [openFilter, setOpenFilter] = useState(false);
+
     return (
         <section className='body-section'>
             <h1>สำรวจชมรม</h1>
             <div className='flex flex-col gap-4 sm:gap-6 w-full'>
                 {/* Top Section */}
-                <div className='flex gap-4 w-full'>
+                <div className='flex gap-2 sm:gap-4 w-full'>
                     <SearchBar className='flex-1' />
-                    {/* Icon Filter Component */}
+                    
+                    {/* Filter Icon */}
+                    <div 
+                        className='w-12 h-12 flex justify-center items-center border border-stroke rounded-[10px] text-text-gray  hover:text-primary hover:border-2 hover:border-primary transition-all'
+                        onClick={() => setOpenFilter(!openFilter)}
+                    >
+                        <SlidersHorizontal />
+                    </div>
                 </div>
+
+                {/* Filter Menu */}
+                {openFilter && (
+                    <div className='w-full border border-stroke rounded-[10px] p-4'>
+                        
+                    </div>
+                )}
                 
                 {/* Clubs Section */}
                 <div className='grid grid-cols-2 lg:grid-cols-3 gap-3 sm:gap-6'>
