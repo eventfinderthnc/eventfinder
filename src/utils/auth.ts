a63a6364d87082f43d092b06775c311822bd457b
Punnaphop Sitthiamornporn <punnaphop.sitthi@gmail.com>
2026-01-26 13:07:01 +0700

    [feat] add google oauth config

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -6,7 +6,15 @@ export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
+  baseURL: process.env.BETTER_AUTH_URL,
   emailAndPassword: {
     enabled: true,
+  },
+  socialProviders: {
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    },
   },
 });
