3f250435490175522dd23f9dd9ba49f01215e728
Naiyarat Hanmatheekuna <admin@Naiyarats-MacBook-Air.local>
2026-02-03 22:10:22 +0700

    [chore] set up migration file and use correct auth schema for better auth

--- src/server/api/routers/calendarItem.ts
+++ src/server/api/routers/calendarItem.ts
@@ -6,63 +6,71 @@ import { CreateCalendarItemRequestSchema, UpdateCalendarItemRequestSchema } from
 import { getTRPCError } from "@/utils/error";
 import { TRPCError } from "@trpc/server";
 import { calendarItem } from "@/server/db/calendarItem";
-import { and, eq, gte, lte, SQL } from "drizzle-orm";
+import { and, eq, gte, lte } from "drizzle-orm";
 import { post } from "@/server/db/post";
 
 export const calendarItemRouter = createTRPCRouter({
-    getByMonth: protectedProcedure.input(
-      z.object({
-        userId: z.string(),
-        month: z.number().gt(0).lt(13),
-        year: z.number().gte(0)
-      })
-    ).query(async ({ input }) => {
-        const startDate = new Date(input.year, input.month - 1, 1);
-        const endDate = new Date(input.year, input.month, 0, 23, 59, 59); // day = 0 means the last day of the previous month and we specify 23:59:59 to make it the end of that day.
-        const [res, error] = await calendarItemServiceImpl.getByMonth(and(eq(calendarItem.userId, input.userId), gte(post.date, startDate), lte(post.date, endDate)));
-        if(error) throw new TRPCError(getTRPCError(error));
-        return res;
-    }),
+	getByMonth: protectedProcedure
+		.input(
+			z.object({
+				userId: z.string(),
+				month: z.number().gt(0).lt(13),
+				year: z.number().gte(0),
+			}),
+		)
+		.query(async ({ input }) => {
+			const startDate = new Date(input.year, input.month - 1, 1);
+			const endDate = new Date(input.year, input.month, 0, 23, 59, 59); // day = 0 means the last day of the previous month and we specify 23:59:59 to make it the end of that day.
+			const [res, error] = await calendarItemServiceImpl.getByMonth(
+				and(eq(calendarItem.userId, input.userId), gte(post.date, startDate), lte(post.date, endDate)),
+			);
+			if (error) throw new TRPCError(getTRPCError(error));
+			return res;
+		}),
 
-    getOneByFilter: protectedProcedure.input(
-      z.object({
-        userId: z.string()
-      })
-    ).query(async ({ input }) => {
-      const [res, error] = await calendarItemServiceImpl.getOneByFilter(eq(calendarItem.userId, input.userId))
-    }),
+	getOneByFilter: protectedProcedure
+		.input(
+			z.object({
+				userId: z.string(),
+			}),
+		)
+		.query(async ({ input }) => {
+			const [res, error] = await calendarItemServiceImpl.getOneByFilter(eq(calendarItem.userId, input.userId));
+		}),
 
-    getAllByFilter: protectedProcedure.input(
-      z.object({
-        userId: z.string()
-      })
-    ).query(async ({ input }) => {
-        const [res, error] = await calendarItemServiceImpl.getByFilter(eq(calendarItem.userId, input.userId));
-        if(error) throw new TRPCError(getTRPCError(error));
-        return res;
-    }),
+	getAllByFilter: protectedProcedure
+		.input(
+			z.object({
+				userId: z.string(),
+			}),
+		)
+		.query(async ({ input }) => {
+			const [res, error] = await calendarItemServiceImpl.getByFilter(eq(calendarItem.userId, input.userId));
+			if (error) throw new TRPCError(getTRPCError(error));
+			return res;
+		}),
 
-    create: protectedProcedure.input(CreateCalendarItemRequestSchema).mutation(async ({ input }) => {
-        const [res, error] = await calendarItemServiceImpl.create(input);
-        if (error) return new TRPCError(getTRPCError(error));
-        return res;
-    }),
+	create: protectedProcedure.input(CreateCalendarItemRequestSchema).mutation(async ({ input }) => {
+		const [res, error] = await calendarItemServiceImpl.create(input);
+		if (error) return new TRPCError(getTRPCError(error));
+		return res;
+	}),
 
-    update: protectedProcedure.input(UpdateCalendarItemRequestSchema).mutation(async ({ input }) => {
-        const res = await calendarItemServiceImpl.update(eq(calendarItem.id, input.id), input);
-        if (res) return new TRPCError(getTRPCError(res));
-        return null;
-    }),
+	update: protectedProcedure.input(UpdateCalendarItemRequestSchema).mutation(async ({ input }) => {
+		const res = await calendarItemServiceImpl.update(eq(calendarItem.id, input.id), input);
+		if (res) return new TRPCError(getTRPCError(res));
+		return null;
+	}),
 
-    delete: protectedProcedure
-        .input(
-            z.object({
-                id: z.number(),
-            }),
-        )
-        .mutation(async ({ input }) => {
-            const res = await calendarItemServiceImpl.delete(eq(calendarItem.id, input.id));
-            if (res) return new TRPCError(getTRPCError(res));
-            return null;
-        }),
+	delete: protectedProcedure
+		.input(
+			z.object({
+				id: z.number(),
+			}),
+		)
+		.mutation(async ({ input }) => {
+			const res = await calendarItemServiceImpl.delete(eq(calendarItem.id, input.id));
+			if (res) return new TRPCError(getTRPCError(res));
+			return null;
+		}),
 });
--- src/server/db/calendarItem.ts
+++ src/server/db/calendarItem.ts
@@ -1,18 +1,18 @@
-import { pgTable, serial, text, timestamp, varchar } from "drizzle-orm/pg-core";
+import { pgTable, serial, timestamp, text } from "drizzle-orm/pg-core";
 import { post } from "./post";
-import { user } from "./user";
+import { user } from "./auth-schema";
 
 export const calendarItem = pgTable("calendar_item", {
-  id: serial("id").primaryKey(),
-  postId: serial("post_id")
-    .references(() => post.id, { onDelete: 'cascade' })
-    .notNull(),
-  userId: text("user_id")
-    .references(() => user.id, { onDelete: 'cascade' })
-    .notNull(),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date())
-    .notNull(),
+	id: serial("id").primaryKey(),
+	postId: serial("post_id")
+		.references(() => post.id, { onDelete: "cascade" })
+		.notNull(),
+	userId: text("user_id")
+		.references(() => user.id, { onDelete: "cascade" })
+		.notNull(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date())
+		.notNull(),
 });
