898be8bb3f90bf2e822b98045c9d8aa83eece260
Nat Silprasert <natsilprasert@gmail.com>
2026-02-12 11:59:32 +0700

    [fix] import wrong path and type error

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -2,25 +2,20 @@ import { TRPCError } from "@trpc/server";
 import { CreateUserRequestSchema, UpdateUserRequestSchema } from "../dto/user.dto";
 import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
 import { getTRPCError } from "@/utils/error";
-import { user } from "@/server/db/user"
+import { user } from "@/server/db/auth-schema"
 import z from "zod";
 import { eq } from "drizzle-orm";
 import { userServiceImpl } from "@/server/api/service/user.service";
 
 export const userRouter = createTRPCRouter({
-    create: publicProcedure.input(CreateUserRequestSchema).mutation(async ({ input }) => {
-        const [res, error] = await userServiceImpl.create(input);
-        if (error) return new TRPCError(getTRPCError(error));
-        return res;
-    }),
-
+    
     update: protectedProcedure.input(UpdateUserRequestSchema).mutation(async ({ input }) => {
         const res = await userServiceImpl.update(eq(user.id, input.id), input);
         if (res) return new TRPCError(getTRPCError(res));
         return null;
     }),
 
-    delete: protectedProcedure.input(z.object({ id: z.number() })).mutation(async ({ input }) => {
+    delete: protectedProcedure.input(z.object({ id: z.string() })).mutation(async ({ input }) => {
         const res = await userServiceImpl.delete(eq(user.id, input.id));
         if (res) return new TRPCError(getTRPCError(res));
         return null;
