12e295fa6be179c034ef1e22f6ac166fe946378f
Nat Silprasert <natsilprasert@gmail.com>
2026-03-16 16:26:12 +0700

    [chore]: add placehold.co to Next.js image remote patterns

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ const config = {
     images: {
         remotePatterns: [
             {
+                protocol: 'https',
+                hostname: 'placehold.co',
+                pathname: "/**",
+            },
+            {
                 protocol: "https",
                 hostname: "lh3.googleusercontent.com",
                 pathname: "/**",
